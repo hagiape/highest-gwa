e6d380b5b4b1d590b5f142f25dc7166097e53191
hagiape <g_chrisf@protonmail.com>
2023-04-22 19:01:57 +0800

    added for loop for the main code

--- highest-gwa.py
+++ highest-gwa.py
@@ -9,6 +9,15 @@ highest_gwa = 5
 # placeholder variable for the name and gwa of student with highest gwa
 top_student = ''
 # for index [2:7] in binary file
+for i in read_gwa:
+    current_gwa = int(i[3:7])
+    if current_gwa > highest_gwa:
+        highest_gwa = current_gwa
+        # when converting the contents binary file as list, after the name of student
+        # you will encounter a backslash character. it will be utilized as the stopping
+        # point of the string
+        backslash = i.find('\\')
+        top_student = i[3:backslash]
 # convert to int
 # if placeholder > index then replace placeholder with index
 # print highest gwa with the name of the student
