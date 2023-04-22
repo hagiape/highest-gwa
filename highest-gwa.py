009ae7206e54acf733261ce108d64bd456775bb8
hagiape <g_chrisf@protonmail.com>
2023-04-22 22:05:28 +0800

    fixed errors encountered during runtime

--- highest-gwa.py
+++ highest-gwa.py
@@ -3,21 +3,18 @@
 # outputs the name of the student who got the highest GWA (including the GWA).
 
 # variable to read binary file
-read_gwa = open("d:College/22-23/2nd-Sem/OOP/assignment/assignment-3/gwa/gwa.bin", "rb")
+read_gwa = open('D:/College/22-23/2nd-Sem/OOP/assignment/assignment-3/highest-gwa/gwa.bin', 'rb')
 # placeholder variable for storing the current highest GWA
 highest_gwa = 5
 # placeholder variable for the name and gwa of student with highest gwa
 top_student = ''
 # for index [2:7] in binary file
 for i in read_gwa:
-    current_gwa = int(i[3:7])
-    if current_gwa > highest_gwa:
+    current_gwa = float(i[0:4])
+    if current_gwa < highest_gwa:
         highest_gwa = current_gwa
-        # when converting the contents binary file as list, after the name of student
-        # you will encounter a backslash character. it will be utilized as the stopping
-        # point of the string
-        backslash = i.find('\\')
-        top_student = i[6:backslash]
+        student = str(i[5:])
+        top_student= student[2:8]
 # print highest gwa with the name of the student
 print(highest_gwa)
 print(top_student)
