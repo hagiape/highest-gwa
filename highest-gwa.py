3acd71759c9bb990fd960ca2306a0283e5ab2c8e
hagiape <g_chrisf@protonmail.com>
2023-04-22 18:57:05 +0800

    added placeholder variables and for reading binary file

--- highest-gwa.py
+++ highest-gwa.py
@@ -3,8 +3,11 @@
 # outputs the name of the student who got the highest GWA (including the GWA).
 
 # variable to read binary file
+read_gwa = open("d:College/22-23/2nd-Sem/OOP/assignment/assignment-3/gwa/gwa.bin", "rb")
 # placeholder variable for storing the current highest GWA
+highest_gwa = 5
 # placeholder variable for the name and gwa of student with highest gwa
+top_student = ''
 # for index [2:7] in binary file
 # convert to int
 # if placeholder > index then replace placeholder with index
