d3716422da0a5ccacf14eaebab6dfef88ef71a5c
hagiape <g_chrisf@protonmail.com>
2023-04-22 18:54:21 +0800

    added pseudocode

--- highest-gwa.py
+++ highest-gwa.py
@@ -1,3 +1,11 @@
 # Write a Python program that read a binary file containing the 
 # name of 20 students together with their GWA. The program will 
 # outputs the name of the student who got the highest GWA (including the GWA).
+
+# variable to read binary file
+# placeholder variable for storing the current highest GWA
+# placeholder variable for the name and gwa of student with highest gwa
+# for index [2:7] in binary file
+# convert to int
+# if placeholder > index then replace placeholder with index
+# print highest gwa with the name of the student
