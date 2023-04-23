0465d871546fd5cb4daf623a9ceaf519ac3e6601
hagiape <g_chrisf@protonmail.com>
2023-04-23 15:20:56 +0800

    imported code snippet of progress bar from stackoverflow

--- highest-gwa.py
+++ highest-gwa.py
@@ -2,6 +2,24 @@
 # name of 20 students together with their GWA. The program will 
 # outputs the name of the student who got the highest GWA (including the GWA).
 
+# the code below is a progress bar, imported from a code snippet
+# credits to  https://stackoverflow.com/a/34482761
+import sys, time
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("{}[{}{}] {}%/{}%\r".format(prefix, "█"*x, "."*(size-x), j, count))
+        file.flush()
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()  
+for i in progressbar(range(100), '\033[32mFinding the student with highest GWA: \033[32m', 50):
+    time.sleep(0.025)
+
 try:
     # variable to read binary file
     read_gwa = open('D:/College/22-23/2nd-Sem/OOP/assignment/assignment-3/highest-gwa/gwa.bin', 'rb')
@@ -15,10 +33,13 @@ try:
         if current_gwa < highest_gwa:
             highest_gwa = current_gwa
             student = str(i[5:])
-            top_student= student[2:8]
+            top_student= student[2:student.find('\\')]
     # print highest gwa with the name of the student
-    print(highest_gwa)
-    print(top_student)
+    print('\033[32m~' * (len(top_student)) + '\033[0m')
+    print('\033[33m  Congratulations!\033[0m')
+    print(str(highest_gwa).center(18))
+    print('\033[36m' + top_student + '\033[36m')
+    print('\033[32m~' * (len(top_student)) + '\033[0m')
     read_gwa.close()
 except:
     print('An error occurred. Please try again.')
