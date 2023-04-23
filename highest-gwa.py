8f8162b574f9b7e61113e47f714aa48f86907b5c
hagiape <g_chrisf@protonmail.com>
2023-04-23 16:30:41 +0800

    removed pseudocode and formatted file path - ready for push

--- highest-gwa.py
+++ highest-gwa.py
@@ -21,20 +21,15 @@ for i in progressbar(range(100), '\033[32mFinding the student with highest GWA: 
     time.sleep(0.025)
 
 try:
-    # variable to read binary file
-    read_gwa = open('D:/College/22-23/2nd-Sem/OOP/assignment/assignment-3/highest-gwa/gwa.bin', 'rb')
-    # placeholder variable for storing the current highest GWA
+    read_gwa = open('gwa.bin', 'rb')
     highest_gwa = 5
-    # placeholder variable for the name and gwa of student with highest gwa
     top_student = ''
-    # for index [2:7] in binary file
     for i in read_gwa:
         current_gwa = float(i[0:4])
         if current_gwa < highest_gwa:
             highest_gwa = current_gwa
             student = str(i[5:])
             top_student= student[2:student.find('\\')]
-    # print highest gwa with the name of the student
     print('\033[32m~' * (len(top_student)) + '\033[0m')
     print('\033[33m  Congratulations!\033[0m')
     print(str(highest_gwa).center(18))
